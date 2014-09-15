53d6801b7d091b3d1855a98478527d6ea040c38c
Hayaki Saito <user@zuse.jp>
2014-09-15 12:36:44 +0900

    xserver-xsdl-sixel

---
+++ xserver-xsdl-sixel.rb
@@ -0,0 +1,48 @@
+require "formula"
+
+class XserverXsdlSixel < Formula
+  homepage "http://cgit.freedesktop.org/xorg/xserver"
+  head "https://github.com/saitoha/xserver-xsdl-SIXEL.git", :branch => "sixel-for-Xquartz"
+
+  depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  depends_on :x11
+  depends_on "pixman"
+  depends_on "sdl-sixel"
+
+  ENV["SDL"] = "yes"
+  ENV["ACLOCAL"] = "aclocal -I #{MacOS::X11.share}/aclocal"
+  ENV["PKG_CONFIG_PATH"] = "#{HOMEBREW_PREFIX}/lib/pkgconfig:#{MacOS::X11.lib}/pkgconfig"
+
+  def install
+    args = ["--prefix=#{prefix}",
+            "--with-xkb-path=#{MacOS::X11.share}/X11/xkb",
+            "--with-xkb-bin-directory=#{MacOS::X11.bin}",
+            "--disable-xorg",
+            "--disable-dmx",
+            "--disable-xvfb",
+            "--disable-xnest",
+            "--disable-xwin",
+            "--disable-xephyr",
+            "--disable-xfake",
+            "--disable-xfbdev",
+            "--disable-glx",
+            "--disable-unit-tests",
+            "--disable-xf86vidmode",
+            "--disable-xquartz",
+            "--disable-docs",
+            "--enable-xsdl",
+            "--enable-kdrive",
+            "--enable-kdrive-kbd",
+            "--enable-kdrive-mouse",
+            "--enable-kdrive-evdev",
+           ]
+
+    system "./autogen.sh"
+    system "./configure", *args
+    system "make", "install"
+  end
+end
